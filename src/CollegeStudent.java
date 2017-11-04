329d1b90920ac75ebe52f8c37714f40f919bde7d
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-04 15:36:20 -0500

    CollegeStudent

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,13 +1,20 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Garrett Weissenborn
+  	Date:   11/4/2017
 */
 
 public class CollegeStudent {
 
 	public static void main(String[] args) {
-
+		int Total_number_of_classes = 5;
+		double Math = 3.5;
+		double English = 3.7;
+		double Health = 3.0;
+		double Autoshop = 4.0;
+		double PE = 3.3;
+		Object GPA = (Math + English + Health + Autoshop + PE) / Total_number_of_classes;
+		System.out.println("My GPA is "+ GPA);
 	}
 
 }
