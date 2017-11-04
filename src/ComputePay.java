15f8d6407e5ba6122547aeb90b4ce8ae273dc171
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-03 19:57:53 -0500

    Compute pay

--- src/ComputePay.java
+++ src/ComputePay.java
@@ -1,18 +1,20 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Garrett Weissenborn
+  	Date:  11/4/2017
 */
 
 public class ComputePay {
 	public static void main(String[] args) {
-		System.out.println("My total hours worked:");
-		System.out.println(4 + 5 + 8 + 4);
+		int hours_worked = 20;
+		double salary_an_hour = 10.50;
+		Object tolal_pay = hours_worked * salary_an_hour;
 		
-		System.out.println("My hourly salary:");
-		System.out.println("$8.75");
-		
-		System.out.println("My total pay:");
-		System.out.println((4 + 5 + 8 + 4) * 8.75);
-	}
+		System.out.println("My total hours worked");
+		System.out.println(hours_worked);
+		System.out.println("My base pay");
+		System.out.println(salary_an_hour);
+		System.out.println("My total pay");
+		System.out.println(tolal_pay);
+	}	
 }
