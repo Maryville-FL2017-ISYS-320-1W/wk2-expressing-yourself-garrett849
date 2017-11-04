bfd517d49f911b78d1180e4dfc7c4f5d4a7472bd
garrett849 <33042042+garrett849@users.noreply.github.com>
2017-11-03 20:57:09 -0500

    ReAssignment 1

--- src/ReAssignment.java
+++ src/ReAssignment.java
@@ -3,12 +3,13 @@
   	Name(s):
   	Date: 
 */
-
+	//Name Garrett Weissenborn
+	//Date 11/4/2017
 /*
  	Your predicted values for variables a, b, and c
-	a => 
-	b =>
-	c => 
+	a => 6 Correct
+	b => 9 Correct
+	c => 16 Correct
  
  	After testing the statements in the main method, indicate above if your predictions were correct.
 */
@@ -17,7 +18,15 @@ public class ReAssignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 5; 
+		int b = 10; 
+		int c = b;
+		a = a + 1;
+		b = b - 1; 
+		c = c + a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
 	}
 
 }
